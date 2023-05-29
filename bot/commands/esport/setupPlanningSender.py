10c864ccf1f8339868e6b66da2604d46d24df45f
Hollitizz <valentin.caure@epitech.eu>
2023-05-29 17:14:45 +0200

    [ADD] PRINT LEAGUES

--- bot/commands/esport/setupPlanningSender.py
+++ bot/commands/esport/setupPlanningSender.py
@@ -50,6 +50,7 @@ async def setupPlanningSender(bot: BotType, ctx: Interaction, channel_id: str):
     leagues: list[dict] = bot.api.getLeagues(
         language
     ).get("data", {}).get("leagues", [])
+    print(league.get("region", "") for league in leagues)
     leagues.sort(key=lambda x: SORT_ORDER.get(
         language, []
     ).index(x.get("region", "")) or -1)
